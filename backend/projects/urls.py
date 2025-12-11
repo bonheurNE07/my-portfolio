2ffca319904de998c1d3eb9550f79b0789c394a2
Bonheur Ndeze <bonheurndezenc@gmail.com>
2025-12-12 00:41:56 +0200

    Add app_name to projects URL configuration

--- backend/projects/urls.py
+++ backend/projects/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import ProjectListView, ProjectDetailView
 
+app_name = 'projects'
+
 urlpatterns = [
     path('', ProjectListView.as_view(), name='project-list'),
     path('<slug:slug>/', ProjectDetailView.as_view(), name='project-detail'),
